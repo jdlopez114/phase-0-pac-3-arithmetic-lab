d50e66b7c89c6b7b6ddfe6caa1752a07413190d7
Josiel Lopez <jdlopez114@gmail.com>
2022-02-02 23:05:30 -0500

    commit tree 4e0af9e8227a1bfcba8f1c89a155c431bd0f7c79

---
+++ index.js
@@ -0,0 +1,49 @@
+let number = 5;
+
+function add(number){
+    return number += 3;
+}
+
+function subtract(){
+    return number -+ 3;
+}
+
+function multiply(){
+    return number *= 3;
+}
+
+function divide(){
+    return number /= 3;
+}
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+function increment(n){
+    return n + 1;
+}
+
+function decrement(n){
+    return n - 1;
+}
+
+function makeInt(string){
+    return parseInt(string,10);
+}
+
+function preserveDecimal(string){
+    return parseFloat(string)
+}
